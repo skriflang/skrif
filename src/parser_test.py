38bdf874921c4ec9746c81f1f3082009d00fe1aa
skriflang <skrif.org@gmail.com>
2025-05-10 14:59:15 +0200

    Complete Day 3: Add conditionals and loops syntax, update lexer and parser, add documentation

--- src/parser_test.py
+++ src/parser_test.py
@@ -12,7 +12,7 @@ class SkrifParser(Parser):
         self.names[p.NAME] = int(p.NUMBER)
         return ('set', p.NAME, p.NUMBER)
 
-    @_('IF NAME GREATER NUMBER THEN PRINT NAME')
+    @_('IF NAME GREATER NUMBER THEN PRINT NAME END')
     def statement(self, p):
         if self.names.get(p.NAME, 0) > int(p.NUMBER):
             return ('print', p.NAME, self.names[p.NAME])
@@ -21,7 +21,7 @@ class SkrifParser(Parser):
 if __name__ == "__main__":
     lexer = SkrifLexer()
     parser = SkrifParser()
-    code = "set x to 5\nif x greater than 3 then print x"
+    code = "set x to 5\nif x greater than 3 then print x end"
     tokens = lexer.tokenize(code)
     result = parser.parse(tokens)
     print(result)
